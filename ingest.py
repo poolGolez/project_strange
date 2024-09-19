0e6c25ec92091121521196927053903662a98fa3
Pool Golez <loop.edward@gmail.com>
2024-09-19 17:52:58 +0800

    Initial implementation for ingestion

--- ingest.py
+++ ingest.py
@@ -1,4 +1,12 @@
+from dotenv import load_dotenv
 from langchain_community.document_loaders import CSVLoader
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
+
+load_dotenv()
+
+INDEX_NAME = "top-movies-gemini-embedding-004"
+embeddings = GoogleGenerativeAIEmbeddings(model="models/text-embedding-004")
 
 
 def ingest(file_path: str):
@@ -9,9 +17,11 @@ def ingest(file_path: str):
         content_columns=["overview"]
     )
     docs = loader.load()
-
-    print("Done.")
+    print(f"Loading {len(docs)} documents to Pinecone...")
+    PineconeVectorStore.from_documents(docs, embeddings, index_name=INDEX_NAME)
+    print(f"Loaded {len(docs)} documents to Pinecone.")
 
 
 if __name__ == "__main__":
-    ingest("resources/movies_100_only.csv")
+    file_path = "resources/movies_100_only.csv"
+    ingest(file_path)
